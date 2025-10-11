82d51b06f89a8456532719cbb7f09bcc87f19aa6
ulybin05 <ulybin05@inbox.ru>
2025-10-11 09:56:37 +0000

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,9 +13,12 @@ interface City {
   standalone: false,
 })
 export class HomePage {
-  MyName: string = "lbn"
+  MyName: string = "Daniel Ulybin"
   MyList: string[] = ["1", "2", "3"]
-  MyCities: 
+  MyCities: City[] = [
+    {name: "Moscow", population: 14},
+    {name:"Saint-Petersburg", population: 7}
+  ]
   constructor() {}
   btn_click(){
     this.MyName = this.MyName + "!"
